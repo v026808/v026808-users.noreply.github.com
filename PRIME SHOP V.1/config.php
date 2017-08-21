a08c9be6921fd6d20f415b2c945e920002bb9414
v026808 <v026808@users.noreply.github.com>
2017-08-21 16:27:59 +0300

    init commit

---
+++ PRIME SHOP V.1/config.php
@@ -0,0 +1,56 @@
+<?php 
+	
+	/* Настройки магазина */
+
+// Название магазина
+$title = "Онлайн магазин ИГР | CHEAP ACCOUNTS";
+
+// Название магазина в логотипе
+$title_logo = "CHEAP ACCOUNTS";
+
+//Описание магазина
+$description = "Онлайн магазин ИГР | PRIMEAREA SHOP. Здесь вы можете купить лицензионные игры и ключи для STEAM, Origin, Uplay, Battle.net, Minecraft.";
+
+//Ключевые слова магазина
+$keywords = "магазин ключей, магазин аккаунтов, Rust, GTA, key, accounts, cs go, dota, купить аккаунт, бесплатные аккаунты, раздача аккаунтов, steam, origin, uplay, купить ключ, купить steam, steam игры, купить ключи steam, купить игры на пк, купить ключ стим";
+
+//Название страницы "О нас"
+$about = "О нас";
+
+//Название страницы "Контакты"
+$contact = "Контакты";
+
+//Название страницы "Гарантии"
+$garant = "Гарантии";
+
+//Название страницы "Отзывы"
+$review = "Отзывы";
+
+	/* Ваши контактные данные */
+
+//Skype
+$skype = "vova-khokhlov2898";
+
+//Емайл адрес
+$email = "admin@cheapaccounts.ru";
+
+//Телефон
+$phone = "+7 (000) 000-00-00";
+
+	/* Ваши комментарии Вконтакте */
+
+//Номер виджета
+$api_id = "<!-- Put this script tag to the <head> of your page -->
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?146"></script>
+
+<script type="text/javascript">
+  VK.init({apiId: 6156420, onlyWidgets: true});
+</script>
+
+<!-- Put this div tag to the place, where the Comments block will be -->
+<div id="vk_comments"></div>
+<script type="text/javascript">
+VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"});
+</script>";
+
+?>
